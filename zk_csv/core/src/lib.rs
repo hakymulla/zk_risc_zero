2d3c271da12bbbd42cb106db8d1d38be44cd0fb6
Kazeem Hakeem <hakymulla@gmail.com>
2024-06-24 10:43:29 +0100

    Delete zk_csv directory

--- zk_csv/core/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use risc0_zkvm::sha::Digest;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize,Deserialize, Eq, PartialEq)]
-pub struct Output {
-    pub first_column_total: i64,
-    pub second_column_total: i64,
-    pub hash: Digest,
-}
